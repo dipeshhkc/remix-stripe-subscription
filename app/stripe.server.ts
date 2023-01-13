67a4219c9d5b19dce7b7806193a7ce778f0dced8
dipesh <kcdipesh429@gmail.com>
2023-01-12 20:19:19 -0600

    feat: setup and checkout stripe

---
+++ app/stripe.server.ts
@@ -0,0 +1,41 @@
+
+import Stripe from 'stripe';
+import { UserWithSubscription } from './user.server';
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2022-11-15',
+});
+
+export const stripeCheckout = async ({
+  priceId,
+  tier,
+  user,
+}: {
+  priceId: string;
+  tier: string;
+  user: UserWithSubscription;
+}): Promise<string> => {
+  const lineItems = [
+    {
+      price: priceId,
+      quantity: 1,
+    },
+  ];
+  const customerData = user?.subscription?.stripe_customer_id
+    ? { customer: user?.subscription?.stripe_customer_id }
+    : { customer_email: user!.email };
+
+  const session = await stripe.checkout.sessions.create({
+    ...customerData,
+    mode: 'subscription',
+    client_reference_id: String(user!.id),
+    payment_method_types: ['card', 'us_bank_account'],
+    line_items: lineItems,
+    success_url: `${process.env.ORIGIN}/dashboard`,
+    cancel_url: `${process.env.ORIGIN}/subscriptions`,
+    allow_promotion_codes: true,
+    metadata: { tier },
+  });
+
+  return session.url!;
+};
